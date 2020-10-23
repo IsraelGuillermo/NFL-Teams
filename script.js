5d2f4a2b51e26e693d9bf2996cdc8f0abfe1ebd4
Darin Wallace <dwalleye66@gmail.com>
2020-10-23 16:57:15 -0500

    htmlJQueryChanges

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+$("#teams").on("change", function() {
+    $("#demo").html("You selected: " + this.value);
+  });
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
