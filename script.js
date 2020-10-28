2f4ae0f28f120b0307b25ad0c17291defa0d3080
Israel Guillermo <israelguillermo22@gmail.com>
2020-10-27 23:04:34 -0500

    updating JS to allow to save favorite team functionanility

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-$("#teams").on("change", function () {
-  var teamSelected = $(this).find("option:selected").text();
-  var baseUrl = "https://gnews.io/api/v4/search?q=";
-  var apiKey = "ecb04637f4c2aa21b0666704531e67ed";
-  $.ajax({
-    url: baseUrl + teamSelected + "&token=" + apiKey,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    displayArticles(response.articles);
-  });
-});
-
 var teams = [];
 var baseURL = "https://www.thesportsdb.com/api/v1/json/1/";
 var baseUrl = "https://gnews.io/api/v4/search?q=";
+var favoriteTeams = [];
 
 $.ajax({
   url: baseURL + "search_all_teams.php?l=NFL",
@@ -32,19 +20,175 @@ $.ajax({
       .text(response.teams[i].strTeam)
       .attr("data-teamId", response.teams[i].idTeam);
   }
+  console.log(teams);
 });
 
+function getNews(teamSelected) {
+  var baseUrl = "https://gnews.io/api/v4/search?&lang=en&q=";
+  var apiKey = "ecb04637f4c2aa21b0666704531e67ed";
+  $.ajax({
+    url: baseUrl + teamSelected + "&token=" + apiKey,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    displayArticles(response.articles);
+  });
+}
+
 $("#teams").on("change", function () {
-  console.log(teams);
+  var teamSelected = $(this).find("option:selected").text();
+  var teamId = $(this).find("option:selected").attr("data-teamId");
+
+  displayGameInfo(teamId);
+  getNews(teamSelected);
+});
+/*
+  $.ajax({
+    url: baseUrl + teamSelected,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    displayArticles(response.articles);
+  });
+  */
+
+// function displayPastGames(events) {
+//   for (var i = 0; i < events.length; i++) {
+//     var div = $("<div>");
+//     $("<h1>")
+//       .text(events[i].strEvent)
+//       .addClass("is-size-5 is-text-centered")
+//       .appendTo(div);
+//     $("<p>")
+//       .text(new Date(events[i].dateEvent).toLocaleDateString())
+//       .addClass("is-size-5")
+//       .prependTo(div);
+//     div.prependTo("#schedule").addClass("ml-5");
+//   }
+// }
+function displaySchedule(events) {
+  $("#dropdown").removeClass("height").addClass("mb-3 mt-3");
+
+  for (var i = 0; i < events.length; i++) {
+    var div = $("<div>").addClass("columns notification");
+    $("<h1>")
+      .text(events[i].dateEvent + " " + events[i].strEvent)
+
+      .addClass("is-size-5")
+      .appendTo(div);
+
+    div.appendTo("#schedule").addClass("ml-5");
+  }
+}
+
+function getTeamNameFromId(teamId) {
+  for (var i = 0; i < teams.length; i++) {
+    if (Object.keys(teams[i])[0] === teamId) {
+      return Object.values(teams[i])[0];
+    }
+  }
+  return "Unable to find team";
+}
+
+function displayPreviousScores(previousGame) {
+  var awayScore = previousGame.intAwayScore;
+  var idAwayTeam = previousGame.idAwayTeam;
+  //TODO:  We need to translate the team id to the team name.
+  // We have an id saved to the teams array we we can lookup
+  console.log("Away:" + getTeamNameFromId(idAwayTeam), awayScore);
+  var homeScore = previousGame.intHomeScore;
+  var idHomeTeam = previousGame.idHomeTeam;
+  var div = $("<div>").addClass("columns ml-5 notification");
+
+  $("<h1>")
+    .text(
+      previousGame.dateEvent +
+        " " +
+        getTeamNameFromId(idAwayTeam) +
+        " " +
+        awayScore +
+        " vs " +
+        getTeamNameFromId(idHomeTeam) +
+        " " +
+        homeScore
+    )
+    .addClass("is-size-5")
+    .appendTo(div);
+
+  div.prependTo("#schedule");
+}
+
+//Script for displaying articles begins here.
+
+function displayArticles(articles) {
+  //$("#dropdown").removeClass("height").addClass("mb-3 mt-3");
+  $("<h1>").text("Team News").appendTo("#tname").addClass("is-size-2");
+  for (var i = 0; i < articles.length; i++) {
+    var div = $("<div>").addClass("notification");
+    var newH1 = $("<h1>").addClass("is-size-5").appendTo(div);
+
+    $("<a>")
+      .attr("target", "_blank")
+      .attr("href", articles[i].url)
+      .text(articles[i].title)
+      .appendTo(newH1);
+    //need to stop word count of description after 100 characters.
+    var description = articles[i].description;
+    if (description.length > 100) {
+      description = description.substring(0, 99).concat("...");
+    }
+
+    //after 100 characters add an elipses.
+
+    $("<p>").text(description).appendTo(div);
+
+    div.appendTo("#tname").addClass("ml-5");
+    console.log();
+  }
+}
+
+$("#save").on("click", function () {
+  var newTeam = $("#teams").find("option:selected").text();
+  // $("<button>")
+  //   .text($("#teams").find("option:selected").text())
+  //   .appendTo("#buttons");
+  favoriteTeams.push(newTeam);
+  saveTeams();
+  renderFavTeams();
+});
+
+function saveTeams() {
+  localStorage.setItem("favoriteTeams", JSON.stringify(favoriteTeams));
+}
+
+function init() {
+  var favTeams = JSON.parse(localStorage.getItem("favoriteTeams"));
+  if (favTeams !== null) {
+    favoriteTeams = favTeams;
+  }
+
+  renderFavTeams();
+}
+
+init();
+
+function renderFavTeams() {
+  $("#buttons").empty();
+  for (var i = 0; i < favoriteTeams.length; i++) {
+    var team = favoriteTeams[i];
+    var button = $("<button>").addClass("favoriteButton");
+
+    button.text(team).appendTo("#buttons");
+  }
+}
+function displayGameInfo(teamId) {
   $("#schedule").empty();
+  $("#tname").empty();
   $("#title").empty();
   $("<h1>").text("Schedule").appendTo("#title").addClass("is-size-2");
 
-  $("<h1>").text("Team News").appendTo("#tname").addClass("is-size-2");
-
-  teamSelected = $(this).find("option:selected").attr("data-teamId");
   $.ajax({
-    url: baseURL + "eventslast.php?id=" + teamSelected,
+    url: baseURL + "eventslast.php?id=" + teamId,
     method: "GET",
   }).then(function (response) {
     //TODO: Get Scores and Get Team Names.  Show team name and the score
@@ -93,102 +237,30 @@ $("#teams").on("change", function () {
     });
 
     $.ajax({
-      url: baseURL + "eventsnext.php?id=" + teamSelected,
+      url: baseURL + "eventsnext.php?id=" + teamId,
       method: "GET",
     }).then(function (response) {
       console.log(response);
       displaySchedule(response.events);
     });
   });
-  /*
-  $.ajax({
-    url: baseUrl + teamSelected,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    displayArticles(response.articles);
-  });
-  */
+}
+$(document).on("click", ".favoriteButton", function (event) {
+  event.preventDefault();
+  getNews($(this).text());
+  displayGameInfo(getTeamIdFromName($(this).text()));
 });
 
-// function displayPastGames(events) {
-//   for (var i = 0; i < events.length; i++) {
-//     var div = $("<div>");
-//     $("<h1>")
-//       .text(events[i].strEvent)
-//       .addClass("is-size-5 is-text-centered")
-//       .appendTo(div);
-//     $("<p>")
-//       .text(new Date(events[i].dateEvent).toLocaleDateString())
-//       .addClass("is-size-5")
-//       .prependTo(div);
-//     div.prependTo("#schedule").addClass("ml-5");
-//   }
-// }
-function displaySchedule(events) {
-  $("#dropdown").removeClass("height").addClass("mb-3 mt-3");
-
-  for (var i = 0; i < events.length; i++) {
-    var div = $("<div>").addClass("columns");
-    $("<h1>")
-      .text(events[i].dateEvent + " " + events[i].strEvent)
-
-      .addClass("is-size-5")
-      .appendTo(div);
-
-    div.appendTo("#schedule").addClass("ml-5");
-  }
-}
-
-function getTeamNameFromId(teamId) {
+function getTeamIdFromName(teamName) {
+  // for (var [teamId, nameOfTeam] of Object.entries(teams)) {
+  //   if (nameOfTeam === teamName) {
+  //     return teamId;
+  //   }
+  // }
   for (var i = 0; i < teams.length; i++) {
-    if (Object.keys(teams[i])[0] === teamId) {
-      return Object.values(teams[i])[0];
+    if (Object.values(teams[i])[0] === teamName) {
+      return Object.keys(teams[i])[0];
     }
   }
-  return "Unable to find team";
-}
-
-function displayPreviousScores(previousGame) {
-  var awayScore = previousGame.intAwayScore;
-  var idAwayTeam = previousGame.idAwayTeam;
-  //TODO:  We need to translate the team id to the team name.
-  // We have an id saved to the teams array we we can lookup
-  console.log("Away:" + getTeamNameFromId(idAwayTeam), awayScore);
-  var homeScore = previousGame.intHomeScore;
-  var idHomeTeam = previousGame.idHomeTeam;
-  var div = $("<div>").addClass("columns ml-5");
-
-  $("<h1>")
-    .text(
-      previousGame.dateEvent +
-        " " +
-        getTeamNameFromId(idAwayTeam) +
-        " " +
-        awayScore +
-        " vs " +
-        getTeamNameFromId(idHomeTeam) +
-        " " +
-        homeScore
-    )
-    .addClass("is-size-5")
-    .appendTo(div);
-
-  div.prependTo("#schedule");
-}
-
-//Script for displaying articles begins here.
-
-function displayArticles(articles) {
-  $("#dropdown").removeClass("height").addClass("mb-3 mt-3");
-
-  for (var i = 0; i < articles.length; i++) {
-    var div = $("<div>");
-    $("<h1>").text(articles[i].title).addClass("is-size-5").appendTo(div);
-
-    $("<p>").text(articles[i].description).appendTo(div);
-
-    div.appendTo("#tname").addClass("ml-5");
-    console.log();
-  }
+  return "unable to find";
 }
